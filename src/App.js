0846817c737356c4b94fa67f132f81b248c99c63
Mark N Carpenter Jr <mark@mncjr.com>
2021-01-22 19:03:08 -0500

    REFACTOR: Full Pomodoro

--- src/App.js
+++ src/App.js
@@ -19,14 +19,20 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      sprint: 25,
-      sprints: 4,
-      break: 15,
-      rest: 5,
-      time: {
-        min: 25,
-        sec: 0
+      sprint: {
+        total: 4,
+        duration: {
+          min: 25,
+          sec: 0
+        }
       },
+      break: {
+        total: 4,
+        duration: {
+          min: 5,
+          sec: 0
+        }
+      }
     }
   }
 
@@ -43,9 +49,8 @@ class App extends React.Component {
               </CardHeader>
               <CardBody>
                 <Time 
-                  minutes={this.state.time.min}
-                  seconds={this.state.time.sec}
-                  sprints={this.state.sprints}
+                  sprint={this.state.sprint}
+                  break={this.state.break}
                 ></Time>
                 <Card>
                   <CardHeader>
@@ -57,8 +62,13 @@ class App extends React.Component {
                       </Col>
                       <Col xs="3">
                         <InputGroup size="sm">
-                          <InputGroupAddon addonType="prepend">Sprints</InputGroupAddon>
-                          <Input defaultValue={this.state.sprints} placeholder="Sprint" />
+                          <InputGroupAddon
+                            addonType="prepend"
+                          >Sprints</InputGroupAddon>
+                          <Input
+                            defaultValue={this.state.sprint.total}
+                            placeholder="Sprint"
+                          />
                         </InputGroup>
                       </Col>
                     </Row>
@@ -67,14 +77,64 @@ class App extends React.Component {
                     <Row>
                       <Col xs="6">
                         <InputGroup size="sm">
-                          <InputGroupAddon addonType="prepend">@</InputGroupAddon>
-                          <Input defaultValue={this.state.time.min} placeholder="Minutes" />
+                          <InputGroupAddon addonType="prepend">Minutes</InputGroupAddon>
+                          <Input
+                            defaultValue={this.state.sprint.duration.min}
+                            placeholder="Minutes"
+                          />
                         </InputGroup>
                       </Col>
                       <Col xs="6">
                         <InputGroup size="sm">
-                          <InputGroupAddon addonType="prepend">@</InputGroupAddon>
-                          <Input defaultValue={this.state.time.sec} placeholder="Seconds" />
+                          <InputGroupAddon addonType="prepend">Seconds</InputGroupAddon>
+                          <Input
+                            defaultValue={this.state.sprint.duration.sec}
+                            placeholder="Seconds"
+                          />
+                        </InputGroup>
+                      </Col>
+                    </Row>
+                  </CardBody>
+                </Card>
+                <Card>
+                  <CardHeader>
+                    <Row>
+                      <Col xs="9">
+                        <CardTitle>
+                          Break
+                        </CardTitle>
+                      </Col>
+                      <Col xs="3">
+                        <InputGroup size="sm">
+                          <InputGroupAddon
+                            addonType="prepend"
+                          >Breaks</InputGroupAddon>
+                          <Input
+                            defaultValue={this.state.break.total}
+                            placeholder="Breaks"
+                          />
+                        </InputGroup>
+                      </Col>
+                    </Row>
+                  </CardHeader>
+                  <CardBody>
+                    <Row>
+                      <Col xs="6">
+                        <InputGroup size="sm">
+                          <InputGroupAddon addonType="prepend">Minutes</InputGroupAddon>
+                          <Input
+                            defaultValue={this.state.break.duration.min}
+                            placeholder="Minutes"
+                          />
+                        </InputGroup>
+                      </Col>
+                      <Col xs="6">
+                        <InputGroup size="sm">
+                          <InputGroupAddon addonType="prepend">Seconds</InputGroupAddon>
+                          <Input
+                            defaultValue={this.state.break.duration.sec}
+                            placeholder="Seconds"
+                          />
                         </InputGroup>
                       </Col>
                     </Row>
@@ -94,30 +154,19 @@ class Time extends React.Component {
     super(props);
 
     this.state = {
-      time: {
-        min: props.minutes,
-        sec: props.seconds,
-        total: null
-      },
+      sprint: props.sprint,
+      break: props.break,
       activateButton: {
         text: 'Start'
       },
       activeTimer: {
         active: false,
-        min: props.minutes,
-        sec: props.seconds,
+        min: null,
+        sec: null,
         total: null
       }
     }
     this.toggleTimer = this.toggleTimer.bind(this);
-  }
-
-  componentDidMount() {
-    
-  }
-
-  componentWillMount() {
-    
   }
 
   tick() {
@@ -179,21 +228,23 @@ class Time extends React.Component {
 
   render() {
     return (
-      <div>
-        <InputGroup>
-          <InputGroupAddon addonType="prepend">
-            <Button
-              color="primary"
-              onClick={this.toggleTimer}
-            >{this.state.activateButton.text}</Button>
-          </InputGroupAddon>
-          <Input defaultValue={this.state.activeTimer.total} readOnly={true} />
-          <InputGroupAddon addonType="append">
-            <Button color="warning">Skip</Button>
-            <Button color="danger">Reset</Button>
-          </InputGroupAddon>
-        </InputGroup>
-      </div>
+      <Card>
+        <CardBody>
+          <InputGroup>
+            <InputGroupAddon addonType="prepend">
+              <Button
+                color="primary"
+                onClick={this.toggleTimer}
+              >{this.state.activateButton.text}</Button>
+            </InputGroupAddon>
+            <Input defaultValue={this.state.activeTimer.total} readOnly={true} />
+            <InputGroupAddon addonType="append">
+              <Button color="warning">Skip</Button>
+              <Button color="danger">Reset</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </CardBody>
+      </Card>
     );
   }
 }
